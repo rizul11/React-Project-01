ddc3d869e08e1e8813e8066c8cbc9803efbc7267
rizul11 <rizulsharma.offical@gmail.com>
2023-08-18 15:46:01 +0530

    changes

--- src/assets/images/Index.js
+++ src/assets/images/Index.js
@@ -52,17 +52,153 @@ import global_nft_right from "../images/global_nft_right.png";
 import hamburger_icon from "../images/hamburger_icon.svg";
 import icons8_play_copy from "../images/icons8-play copy.svg";
 import image_24 from "../images/image 24.png";
-import dece from "../images/keep_bg.png";
-import dece from "../images/dece.png";
-import dece from "../images/dece.png";
-import dece from "../images/dece.png";
-import dece from "../images/dece.png";
-import dece from "../images/dece.png";
-import dece from "../images/dece.png";
-import dece from "../images/dece.png";
-import dece from "../images/dece.png";
+import keep_bg from "../images/keep_bg.png";
+import keep_bg_1 from "../images/keep_bg_1.png";
+import mobile_header_bg from "../images/mobile_header_bg.png";
+import nba from "../images/nba.png";
+import nft from "../images/nft.svg";
+import pattern33 from "../images/pattern3 3.png";
+import pattern36 from "../images/pattern3 6.png";
+import pattern3_0_left from "../images/pattern3_0_left.png";
+import pattern3_1 from "../images/pattern3_1.png";
+import pattern3_1_right from "../images/pattern3_1_right.png";
+import pattern3_3 from "../images/pattern3_3.png";
+import pattern3_4_listen from "../images/pattern3_4_listen.png";
+import pattern3_6 from "../images/pattern3_6.png";
+import pattern3_6_listen from "../images/pattern3_6_listen.png";
+import pexels_pixabay_358319 from "../images/pexels-pixabay-358319.jpg";
+import plus from "../images/plus.png";
+import price_divider from "../images/price_divider.png";
+import question_left from "../images/question_left.png";
+import question_right from "../images/question_right.png";
+import quote from "../images/quote.svg";
+import sandbox from "../images/sandbox.png";
+import services_1 from "../images/services_1.png";
+import services_2 from "../images/services_2.png";
+import services_3 from "../images/services_3.png";
+import social01 from "../images/social01.svg";
+import social02 from "../images/social02.svg";
+import social03 from "../images/social03.svg";
+import social04 from "../images/social04.svg";
+import social05 from "../images/social05.svg";
+import speakers01 from "../images/speakers01.png";
+import speakers02 from "../images/speakers02.png";
+import speakers03 from "../images/speakers03.png";
+import speakers04 from "../images/speakers04.png";
+import speakers05 from "../images/speakers05.png";
+import speakers06 from "../images/speakers06.png";
+import unsplash_01 from "../images/unsplash_01.png";
+import unsplash_02 from "../images/unsplash_02.png";
+import unsplash_03 from "../images/unsplash_03.png";
+import unsplash_04 from "../images/unsplash_04.png";
+import unsplash_05 from "../images/unsplash_05.png";
+import unsplash_06 from "../images/unsplash_06.png";
+import unsplash_07 from "../images/unsplash_07.png";
+import unsplash_08 from "../images/unsplash_08.png";
+import unsplash_09 from "../images/unsplash_09.png";
+import video_image from "../images/video_image.png";
+import wax from "../images/wax.png";
 
 const Icons = {
-
-}
+  Alien,
+  Application,
+  Bitmap,
+  CombinedShapeCopy,
+  Fill1Copy3,
+  Fill1,
+  Fill11,
+  Group16114,
+  Group17Copy3,
+  Group19321,
+  Group19339,
+  Growth,
+  IMG_33241,
+  IMG_34771,
+  IMG_37011,
+  Our_team01,
+  Our_team02,
+  Our_team03,
+  Oval_Tech,
+  Presented,
+  Refresh,
+  Reply,
+  TheGlobalNFTSummi1,
+  TheGlobalNFTSummi,
+  Tickets_Icon01,
+  Tickets_Icon02,
+  Tickets_Icon03,
+  Updates01,
+  Updates02,
+  Updates03,
+  Upload,
+  Vector1,
+  Vector,
+  Wishlist,
+  br,
+  buy_ticket,
+  buy_tickets,
+  buy_tickets_bad,
+  buy_tickets_bottom_mobile,
+  buy_tickets_left,
+  buy_tickets_right,
+  buy_tickets_top_mobile,
+  close_icon,
+  dece,
+  down_arrow,
+  footer_logo,
+  free_poap_1,
+  free_poap_2,
+  free_poap_3,
+  global_nft_left,
+  global_nft_right,
+  hamburger_icon,
+  icons8_play_copy,
+  image_24,
+  keep_bg,
+  keep_bg_1,
+  mobile_header_bg,
+  nba,
+  nft,
+  pattern33,
+  pattern36,
+  pattern3_0_left,
+  pattern3_1,
+  pattern3_1_right,
+  pattern3_3,
+  pattern3_4_listen,
+  pattern3_6,
+  pattern3_6_listen,
+  pexels_pixabay_358319,
+  plus,
+  price_divider,
+  question_left,
+  question_right,
+  quote,
+  sandbox,
+  services_1,
+  services_2,
+  services_3,
+  social01,
+  social02,
+  social03,
+  social04,
+  social05,
+  speakers01,
+  speakers02,
+  speakers03,
+  speakers04,
+  speakers05,
+  speakers06,
+  unsplash_01,
+  unsplash_02,
+  unsplash_03,
+  unsplash_04,
+  unsplash_05,
+  unsplash_06,
+  unsplash_07,
+  unsplash_08,
+  unsplash_09,
+  video_image,
+  wax,
+};
 export default Icons;
