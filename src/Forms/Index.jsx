9c49cc965df16044b3c9a2a1caecc652a91707bb
rizul11 <rizulsharma.offical@gmail.com>
2023-08-22 12:32:40 +0530

    .

--- src/Forms/Index.jsx
+++ src/Forms/Index.jsx
@@ -1,13 +1,10 @@
-import React from "react";
 import * as Yup from "yup";
 
 export const signUpSchema = Yup.object({
   name: Yup.string().min(3).max(18).required("Please enter your name"),
   email: Yup.string().email().required("Please enter your email"),
+  password: Yup.string().min(6).required("Please enter your password"),
+  confirm_password: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password"), null], "Password must match"),
 });
-
-function Index() {
-  return <div></div>;
-}
-
-export default Index;
