bddfd660053cba256d62d21689dae8b0ce0b3360
rizul11 <rizulsharma.offical@gmail.com>
2023-08-22 12:26:33 +0530

    .

--- src/Forms/Index.jsx
+++ src/Forms/Index.jsx
@@ -1,19 +1,13 @@
-import React from 'react'
-import * as Yup from 'yup';
+import React from "react";
+import * as Yup from "yup";
 
-
-
+export const signUpSchema = Yup.object({
+  name: Yup.string().min(3).max(18).required("Please enter your name"),
+  email: Yup.string().email().required("Please enter your email"),
+});
 
 function Index() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
 
-
-
-
-
-export default Index
+export default Index;
